649c1856c4d9a662095fc701b2d65989691edafd
lmy12367 <lfuturemy@163.com>
2025-06-22 11:52:14 +0800

    20题两种解法

---
+++ 20.py
@@ -0,0 +1,41 @@
+##解题方法1
+
+def isValid(self, s: str) -> bool:
+        stack=[]
+        dic={')':'(',']':'[','}':'{'}
+
+        for char in s:
+            if char in dic.values():
+                stack.append(char)
+            elif char in dic.keys():
+                if stack==[] or dic(char) != stack.pop():
+                    return False
+            else:
+                return False
+        
+        return stack==[]
+    
+
+def isValid1(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+        
+        stack = []
+        for c in s:
+            if c == '(' or c == '[' or c == '{':
+                stack.append(c)
+            else:
+                if len(stack) == 0:
+                    return False
+                temp = stack.pop()
+                if c == ')':
+                    if temp != '(':
+                        return False
+                elif c == ']':
+                    if temp != '[':
+                        return False
+                elif c == '}':
+                    if temp != '{':
+                        return False
+        
+        return True if len(stack) == 0 else False
