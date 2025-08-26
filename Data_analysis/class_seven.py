25a761630b6234134d1bd3607dd8c81419658739
lmy12367 <lfuturemy@163.com>
2025-08-26 21:16:03 +0800

    pandas series

--- Data_analysis/class_seven.py
+++ Data_analysis/class_seven.py
@@ -1,2 +1,17 @@
 ##2025年8月26日
 ##panadas开始
+import pandas as pd
+s=pd.Series([10,25,38,44,59])
+print(s)
+
+s=pd.Series([10,25,40,58,6],index=['a','b','c','d','e'])
+print(s)
+
+s = pd.Series([10, 25, 40, 58, 6], index=['a', 'b', 'c', 'd', 'e'], name='月份')
+print(s)
+
+s=pd.Series({'a':1,'b':2})
+print(s)
+s1=pd.Series(s,index=['b'])
+print(s1)
+
