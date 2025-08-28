60d6e9f2b2b578d69e4bef5d955e1949a245dfff
lmy12367 <lfuturemy@163.com>
2025-08-28 21:34:52 +0800

    dropna

---
+++ Data_analysis/class_ten.py
@@ -0,0 +1,20 @@
+##2025.8.28
+import numpy as np
+import pandas as pd
+
+s=pd.Series([1,2,np.nan,None,pd.NA])
+print(s)
+
+df=pd.DataFrame([[1,pd.NA,2],[2,3,5],[None,4,6]],columns=['第一列','第二列','第三列'])
+print(df)
+# print(s.isna())
+# print(df.isna())
+# print(df.isna().sum())
+# print(s.isna().sum())
+print('x'*30)
+print(s.dropna())
+print(df.dropna())
+print(df.dropna(how='all'))
+print(df.dropna(thresh=2))
+print(df.dropna(axis=1))
+print(df.dropna(subset='第一列'))
