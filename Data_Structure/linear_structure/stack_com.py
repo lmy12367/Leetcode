65e8155e6cfbb283779fb40e3db3e31b5d67874d
lmy12367 <lfuturemy@163.com>
2025-08-30 22:39:02 +0800

    stack

--- Data_Structure/linear_structure/stack_com.py
+++ Data_Structure/linear_structure/stack_com.py
@@ -1,2 +1,21 @@
 ##2025.8.29
 ##关于栈的复习
+class mystack:
+    def __init__(self):
+        self.items=[]
+    
+    def isempty(self):
+        return self.items==[]
+    
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items)-1]
+    
+    def size(self):
+        return len(self.items)
+    
