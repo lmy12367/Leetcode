9cb1a41885837ef9a89ef784b68019cec48dfaf4
lmy12367 <lfuturemy@163.com>
2025-09-17 20:24:33 +0800

    al3

---
+++ Data_analysis/data_anal3.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+data = {
+    "name":['alice','alice','bob','alice','jack','bob'],
+    "age":[26,25,30,25,35,30],
+    'city':['NY','NY','LA','NY','SF','LA']
+}
+df = pd.DataFrame(data)
+print(df)
+print(df.duplicated())
+print(df.drop_duplicates())
+print(df.drop_duplicates(subset=['name']))
+print(df.drop_duplicates(subset='age'))
+print(df.drop_duplicates(subset=['name'],keep='last'))
+
+df=pd.read_csv('./data/data_anal/sleep.csv')
+print(df.dtypes)
+df['age']=df['age'].astype('int16')
+print(df.dtypes)
+print(df.gender)
+df['gender']=df['gender'].astype('category')
+print(df.gender)
+df['is_male']=df['gender'].map({'Female':False,"Male":True})
+print(df.is_male)
