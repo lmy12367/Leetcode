e721bf61d6cd84ed5eec573a1f9d2de6387458c5
lmy12367 <lfuturemy@163.com>
2025-09-18 15:20:02 +0800

    2.py

---
+++ 2.py
@@ -0,0 +1,32 @@
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy=ListNode(0)
+        curr=dummy
+        carry=0
+
+        while l1 or l2 or carry:
+            val1=l1.val if l1 else 0
+            val2=l2.val if l2 else 0
+            total=val1+val2+carry
+
+            carry=total//10
+            curr.next=ListNode(total%10)
+            curr=curr.next
+
+            if l1:
+                l1=l1.next
+            if  l2:
+                l2=l2.next
+            
+        return dummy.next
+
+l1=[2,4,3]
+l2=[5,6,4]
+s1=Solution.addTwoNumbers(l1,l2)
+print(s1)
+    
