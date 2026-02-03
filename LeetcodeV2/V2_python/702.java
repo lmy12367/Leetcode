594ee0972b04b7dc2e00f187a2a662fcf697eedc
lmy12367 <lfuturemy@gmail.com>
2026-02-03 20:46:28 +0800

    702

---
+++ LeetcodeV2/V2_python/702.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        
+        int[] nums1 = {-1, 0, 3, 5, 9, 12};
+        System.out.println(solution.search(nums1, 9)); 
+        
+        int[] nums2 = {-1, 0, 3, 5, 9, 12};
+        System.out.println(solution.search(nums2, 2)); 
+    }
+}
