7969cefe26696133f2390e8519dd8c650eccdb14
lmy12367 <lfuturemy@gmail.com>
2026-02-07 21:36:33 +0800

    27 up

--- LeetcodeV2/V2_python/27.py
+++ LeetcodeV2/V2_python/27.py
@@ -11,3 +11,23 @@ class Solution:
                 
 
         return slow
+    
+
+if __name__ == "__main__":
+    solution = Solution()
+
+
+    nums1 = [3, 2, 2, 3]
+    val1 = 3
+    k1 = solution.removeElement(nums1, val1)
+    print(f"案例 1 返回长度: {k1}")
+
+    print(f"案例 1 修改后的前 k 个元素: {nums1[:k1]}")
+    print("-" * 20)
+
+
+    nums2 = [0, 1, 2, 2, 3, 0, 4, 2]
+    val2 = 2
+    k2 = solution.removeElement(nums2, val2)
+    print(f"案例 2 返回长度: {k2}")
+    print(f"案例 2 修改后的前 k 个元素: {nums2[:k2]}")
