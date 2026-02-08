8526456b9bf3f80f7c91ca02c8b91d656ec02b41
lmy12367 <lfuturemy@gmail.com>
2026-02-08 22:36:46 +0800

    977

--- LeetcodeV2/V2_python/977.py
+++ LeetcodeV2/V2_python/977.py
@@ -3,22 +3,20 @@ from typing import List
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        # 初始化结果列表，长度与原数组一致
         res = [0] * n
         
-        # 定义左右双指针
         i, j = 0, n - 1
-        # count 指向结果列表的末尾，从大到小填充
+
         count = n - 1
         
         while i <= j:
             left_square = nums[i] * nums[i]
             right_square = nums[j] * nums[j]
             
-            # 比较左右两端平方的大小
+
             if left_square > right_square:
                 res[count] = left_square
-                i += 1  # 左指针向右移
+                i += 1  
             else:
                 res[count] = right_square
                 j -= 1  
@@ -27,7 +25,7 @@ class Solution:
             
         return res
 
-# --- 主函数测试 ---
+
 if __name__ == "__main__":
     sol = Solution()
     
